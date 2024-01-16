0bde140701e8f522dcb07736198bf37ca14bbc56
hnadiminty <154002588+hnadiminty@users.noreply.github.com>
2024-01-17 00:20:47 +0530

    Add files via upload

---
+++ templates/app.py
@@ -0,0 +1,21 @@
+import flask
+from flask import Flask
+from flask import render_template, request,jsonify
+from chat import get_response
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET"])
+def index_get():
+    return render_template("base.html")
+    
+@app.post("/predict")
+def predict():
+    text = request.get_json().get("message")
+    response = get_response(text)
+    message = {"answer": response}
+    return jsonify(message)
+
+
+if __name__=="__main__":
+    app.run(debug=False)
